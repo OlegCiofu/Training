00b1cc95b72932af1d9ec50a6c1d4ddc013aa60a
OlegCiofu <ciofu.oleg@gmail.com>
2018-06-15 16:54:19 +0300

    First commit

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try1:
+            Console.Write("Please, enter digit to calculate factorial: ");
+            try
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("{0}! = {1}", x, factorial(x));
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number. Please try again");
+                Console.ReadLine();
+                goto try1;
+            }
+            goto try1;
+        }
+
+        static int factorial(int x)
+        {
+            int result;
+
+            if (x == 1)
+                return x;
+
+            result = x * factorial(x - 1);
+            return result;
+        }
+    }
+}
