d00c08d9419429eb1b1131b21c45377f158bbf93
OlegCiofu <ciofu.oleg@gmail.com>
2018-06-15 19:13:16 +0300

    Concat and sort by Methods

---
+++ ConcatAndSort/ConcatAndSort/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace ConcatAndSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods method = new Methods();
+            int[] arr1 = method.EnterArray("First");
+            method.ShowArr(arr1, "First");
+
+            int[] arr2 = method.EnterArray("Second");
+            method.ShowArr(arr2, "Second");
+
+            int[] arr3 = method.Concat(arr1, arr2);
+            method.ShowArr(arr3, "Concatenated");
+
+            int[] arr4 = method.Sort(arr3);
+            method.ShowArr(arr4, "Sorted");
+            
+        }
+    }
+
+    class Methods
+    {
+        public int[] EnterArray(string counter)
+        {
+            Console.WriteLine("Please, enter range of {0} array: ", counter);
+            int n = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine("Please, enter elements of {0} array: ", counter);
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = Int32.Parse(Console.ReadLine());
+            return arr;
+        }
+        public void ShowArr(int[] arr, string counter)
+        {
+            Console.WriteLine("Your {0} array  is: ", counter);
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write("{0} ", arr[i]);
+            Console.ReadLine();
+        }
+
+        public int[] Concat (int[] input1, int[] input2)
+        {
+            int[] arr = new int[input1.Length + input2.Length];
+            for (int i=0; i<arr.Length; i++)
+            {
+                if (i < input1.Length)
+                    arr[i] = input1[i];
+                else
+                    arr[i] = input2[i - input1.Length];
+            }
+            return arr;
+        }
+
+        public int[] Sort (int[] input)
+        {
+            int[] arr = input;
+            int poz = 0;
+            int min = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                min = arr[i];
+                for(int j=i; j<arr.Length;j++)
+                {
+                    if (min > arr[j])
+                    {
+                        min = arr[j];
+                        poz = j;
+                    }
+                }
+                int temp = arr[i];
+                arr[i] = min;
+                arr[poz] = temp;
+            }
+            return arr;
+
+        }
+    }
+
+
+}
